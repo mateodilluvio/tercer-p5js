3fbe3c6dc8c9c1bfea7c12bfd2897d95fdc3d2dd
Mateo <mateodilluvio@gmail.com>
2023-05-29 12:40:40 -0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,239 @@
+
+function setup() {
+  createCanvas(800, 800);
+  background(35, 37, 49);
+
+  //rosa arriba izq
+  fill(248, 160, 219);
+  stroke(248, 160, 219)
+  ellipse(80, 115, 50, 50);
+
+  //rosa entre azul y negro
+  fill(248, 160, 219);
+  stroke(255, 255, 255, 140)
+  ellipse(410, 140, 130, 130);
+
+  //amarillo arriba izq
+  fill(249, 234, 70)
+  stroke(249, 234, 70)
+  ellipse(60, 40, 23, 23);
+
+
+  //el big blue
+  stroke(255, 255, 255, 100);
+  fill(16, 58, 174, 200);
+  ellipse(330, 300, 400, 400);
+
+  //negro sobre azul
+  fill(0, 0, 0, 200);
+  stroke(255, 255, 255, 100);
+  ellipse(277, 247, 250, 250);
+
+
+  //gris? medio izq 
+  fill(179, 193, 219);
+  stroke(0, 0, 0);
+  strokeWeight(2)
+  ellipse(60, 450, 70, 70);
+
+  //negro sobre gris medio izq
+  fill(0, 0, 0);
+  stroke(0, 0, 0);
+  ellipse(74, 434, 20, 20)
+
+  //rojo sobre amarillo medio izq
+  fill(226, 50, 86)
+  stroke(226, 50, 86)
+  ellipse(135, 547, 60, 60)
+
+  //amarillo medio izq
+  fill(252, 233, 85, 210)
+  stroke(252, 233, 85, 210)
+  ellipse(100, 540, 60, 60)
+
+  //naranja sobre naranja medio izq
+  fill(204, 106, 68, 230)
+  stroke(204, 106, 68, 230)
+  ellipse(188, 510, 60, 60)
+
+  //naranja sobre rojo medio izq
+  fill(204, 106, 68, 200)
+  stroke(204, 106, 68, 200)
+  ellipse(175, 549, 60, 60)
+
+  //amarillo sobre naranja
+  fill(213, 167, 47, 225)
+  stroke(213, 167, 47, 225)
+  ellipse(160, 465, 60, 60)
+
+  //chiquito izquierda del azul
+  fill(0, 0, 0);
+  stroke(249, 234, 70);
+  strokeWeight(3);
+  ellipse(140, 367, 20, 20);
+
+  //Medio Abajo sobre azul
+  fill(172, 184, 223, 160);
+  stroke(172, 184, 223, 160);
+  strokeWeight(0);
+  ellipse(320, 500, 80, 80);
+
+  //chiquito medio abajo
+  fill(0, 0, 0);
+  stroke(172, 184, 223, 160);
+  strokeWeight(2);
+  ellipse(300, 547, 17, 17);
+
+  //amarillo en azul, medio
+  strokeWeight(0)
+  fill(250, 227, 124, 200);
+  stroke(250, 227, 124, 200);
+  ellipse(370, 415, 80, 80);
+
+  //circulo pequeño sobre negro, medio
+  fill(166, 225, 183, 200);
+  stroke(166, 225, 183, 200);
+  ellipse(340, 346, 30, 30);
+
+  //circulo sobre negro, medio
+  fill(166, 225, 183, 200);
+  stroke(166, 225, 183, 200);
+  ellipse(400, 336, 75, 75);
+
+  //Naranja sobre big blue
+  fill(222, 164, 45, 180);
+  stroke(222, 164, 45, 180);
+  ellipse(450, 420, 140, 140);
+
+  //rosa sobre naranja medio
+  fill(240, 118, 177, 180);
+  stroke(240, 118, 177, 180);
+  ellipse(490, 465, 50, 50);
+
+  //negro sobre rosa
+  fill(0, 0, 0);
+  stroke(0, 0, 0);
+  ellipse(465, 476, 17, 17);
+
+  //celeste bajo naranja medio
+  fill(83, 187, 222, 220);
+  stroke(83, 187, 222, 220);
+  ellipse(430, 530, 105, 105)
+
+  //verde arriba der
+  fill(59, 119, 109, 200);
+  stroke(59, 119, 109, 200);
+  ellipse(570, 220, 150, 150);
+
+  //circulo negro dentro de verde der
+  fill(0, 0, 0);
+  strokeWeight(8);
+  ellipse(570, 220, 30, 30);
+
+  //amarillo arriba der
+  fill(255, 233, 147);
+  stroke(255, 233, 147);
+  strokeWeight(0);
+  ellipse(576, 100, 60, 60);
+
+  //naranja arriba der
+  fill(244, 170, 21);
+  stroke(255, 233, 147);
+  strokeWeight(2);
+  ellipse(650, 140, 27, 27);
+
+  //rojo arriba der
+  fill(224, 49, 62);
+  stroke(224, 49, 62);
+  strokeWeight(0)
+  ellipse(700, 85, 24, 24);
+
+  //azul abajo der
+  fill(28, 86, 221);
+  stroke(255, 255, 255, 150);
+  strokeWeight(3);
+  ellipse(710, 447, 24, 24);
+
+  //gris? abajo der
+  fill(192, 203, 221, 240);
+  stroke(0, 0, 0)
+  strokeWeight(0)
+  ellipse(670, 590, 150, 150)
+
+  //negro sobre gris der
+  fill(0, 0, 0);
+  stroke(255, 255, 255, 180);
+  strokeWeight(3)
+  ellipse(691, 531, 11, 11);
+
+  //rojo sobre gris der
+  fill(204, 20, 32);
+  stroke(255, 255, 255, 200);
+  ellipse(672, 528, 18, 18);
+
+  //celeste sobre gris der
+  fill(140, 216, 222)
+  stroke(255, 255, 255, 100)
+  strokeWeight(8)
+  ellipse(635, 567, 45, 45);
+
+  //marron sobre gris der
+  fill(198, 175, 132, 240);
+  stroke(198, 175, 132, 240);
+  strokeWeight(0);
+  ellipse(578, 545, 70, 70);
+
+  //amarillo bajo gris der
+  fill(210, 172, 61);
+  stroke(210, 172, 61);
+  ellipse(750, 580, 18, 18);
+
+  //amarillo2 bajo gris der
+  fill(237, 211, 21)
+  stroke(237, 211, 21)
+  ellipse(638, 682, 20, 20);
+
+  //rojo bajo amarillo der
+  fill(211, 49, 81);
+  stroke(211, 49, 81);
+  ellipse(615, 730, 35, 35)
+
+  //rojo bajo amarillo der
+  fill(57, 136, 184);
+  stroke(57, 136, 184);
+  ellipse(705, 760, 25, 25);
+
+  //rosa abajo izq
+  fill(246, 153, 206)
+  stroke(255, 255, 255, 100)
+  strokeWeight(2)
+  ellipse(70, 630, 23, 23)
+
+  //azul abajo izq
+  fill(60, 163, 227);
+  stroke(60, 163, 227);
+  strokeWeight(0);
+  ellipse(110, 680, 48, 48);
+
+  //negro abajo izq
+  fill(0, 0, 0)
+  stroke(202, 213, 146)
+  strokeWeight(3)
+  ellipse(58, 700, 17, 17)
+}
+var angulo = 0;
+var desplazamiento = 220; //es como un translate.
+var escalar = 0; //es el "tamaño" del círculo.
+var velocidad = 3;
+
+// var x = desplazamiento + cos(angulo) * escalar;
+// y para 'y' es lo mismo.
+
+function draw() {
+  fill(random(255), random(200), random(200));
+  var x = desplazamiento + cos(angulo) * escalar;
+  var y = desplazamiento + sin(angulo) * escalar;
+  ellipse(x, y, 40, 40);
+  angulo += velocidad;
+  escalar += 0.3;
+}
